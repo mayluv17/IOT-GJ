0adcead3092772830cb9015d13a0a71d29aa1d15
mayluv17 <mayowaibrahim009@gmail.com>
2024-11-09 16:51:28 +0200

    add moisture graph

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -3,10 +3,12 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { Progress } from "react-circle-progress-bar";
+
 import Image from "next/image"
 import { format, formatRelative } from 'date-fns';
 
 import { eu } from 'date-fns/locale';
+import { ResponsiveLine } from '@nivo/line'
 
 const fetchMoistureData = async () => {
   const response = await axios.get("/api/moisture");
@@ -14,6 +16,7 @@ const fetchMoistureData = async () => {
 };
 
 const progressStyles = {};
+
 
 const isMoreThan3DaysAgo = (timestamp: string): boolean => {
   const threeDaysInMilliseconds = 3 * 24 * 60 * 60 * 1000; // 3 days in milliseconds
@@ -50,15 +53,27 @@ export default function Home() {
 
   const { data, isLoading, error } = useQuery(getMoistureQuery);
 
+
+  const graphData = !isLoading && !error && [{
+    "id": "mostureGraph",
+    "color": "hsl(184, 70%, 50%)",
+    "data": data.map(({ timestamp, moistureAfter, moistureBefore }) => ({
+    "x": timestamp,           // Rename timestamp to x
+    "y": moistureAfter       // Rename moistureAfter to y
+  }))}]
+
+
   return (
     <>
     <p className="p-4 pl-8 bold font-bold text-xl text-blue-600">Plantly<small className="text-slate-600 text-xs ml-1 font-thin">Dashboard</small></p>
       <hr />
-    <div className="m-8 flex flex-wrap gap-8 font-[family-name:var(--font-geist-sans)] ">
+    <div className="m-8 max-w-[690px] mx-auto flex flex-wrap gap-8 font-[family-name:var(--font-geist-sans)] ">
+      
+      {isLoading && <p className="m-auto">Loading...{}</p>}
+      {error && <p className="m-auto text-red-600 text-center">There was an error, it's not you it's us.<br /><i>please refresh</i></p>}
+      {!isLoading && !error && data && (
       <div className="border-2 border-slate-300 p-2 rounded-lg text-center">
         <h4 className="bg-blue-500 font-light text-white p-1 px-3 rounded-2xl w-fit text-sm mx-auto">Moisture Before</h4>
-
-        {!isLoading && !error && data && (
           <Progress
           reduction={0}
           transitionDuration={0.5}
@@ -70,13 +85,14 @@ export default function Home() {
             style={progressStyles}
             progress={data.at(-1).moistureBefore}
           />
+      </div>
         )}
 
 
-      </div>
+      {!isLoading && !error && data && (
       <div className="border-2 border-slate-300 p-2 rounded-lg text-center">
         <h4 className="bg-blue-500 font-light text-white p-1 px-3 rounded-2xl w-fit text-sm mx-auto">Moisture After</h4>
-        {!isLoading && !error && data && (
+        
           <Progress
           reduction={0}
           transitionDuration={0.5}
@@ -88,23 +104,95 @@ export default function Home() {
             style={progressStyles}
             progress={data.at(-1).moistureAfter}
           />
-        )}
       </div>
+        )}
 
+        {!isLoading && !error && data && (
       <div className="border-2 border-slate-300 p-2 rounded-lg text-center">
         <h4 className="flex bg-blue-500 font-light text-white p-1 px-3 rounded-2xl w-fit text-sm mr-auto ml-auto">Plant Mood</h4>
-        {!isLoading && !error && data && 
-        (
+        
         
         <Image alt="happyplant" src={isMoreThan3DaysAgo(data.at(-1).timestamp) ? "/sad.png" : "/happy.png"} width={120} height={160}/>
-        )}
       </div>
+        )}
 
+      {!isLoading && !error && data && (<div className="h-64 w-96">
+        <ResponsiveLine
+        data={graphData}
+        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+        xScale={{ type: 'point' }}
+        yScale={{
+            type: 'linear',
+            min: 'auto',
+            max: 'auto',
+            stacked: true,
+            reverse: false
+        }}
+        yFormat=" >-.2f"
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: 'moisture after against time',
+            legendOffset: 36,
+            legendPosition: 'middle',
+            truncateTickAt: 0
+        }}
+        axisLeft={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: 'count',
+            legendOffset: -40,
+            legendPosition: 'middle',
+            truncateTickAt: 0
+        }}
+        pointSize={10}
+        pointColor={{ theme: 'background' }}
+        pointBorderWidth={2}
+        pointBorderColor={{ from: 'serieColor' }}
+        pointLabel="data.yFormatted"
+        pointLabelYOffset={-12}
+        enableTouchCrosshair={true}
+        useMesh={true}
+        legends={[
+            {
+                anchor: 'bottom-right',
+                direction: 'column',
+                justify: false,
+                translateX: 100,
+                translateY: 0,
+                itemsSpacing: 0,
+                itemDirection: 'left-to-right',
+                itemWidth: 80,
+                itemHeight: 20,
+                itemOpacity: 0.75,
+                symbolSize: 12,
+                symbolShape: 'circle',
+                symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                effects: [
+                    {
+                        on: 'hover',
+                        style: {
+                            itemBackground: 'rgba(0, 0, 0, .03)',
+                            itemOpacity: 1
+                        }
+                    }
+                ]
+            }
+        ]}
+    />
+    </div>
+  
+  )}
+      {!isLoading && !error && data && (
       <div className="grid h-fit">
       <h4 className="flex bg-blue-500 font-light text-white p-1 px-3 rounded-2xl w-fit text-sm h-fit">Last Irrigation</h4>
       <p className="font-light text-5xl text-slate-600 mt-2">{data && formatTimestamp(data?.at(-1).timestamp)}</p>
 
-    </div>
+    </div>)}
     </div>
   
     </>
