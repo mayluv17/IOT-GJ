8ab6db78958a52fa1f28124df385c11d1d492f28
ibrahim abdulganiy <mayowaibrahim009@gmail.com>
2024-12-08 17:36:18 +0200

    fix build error

--- app/src/app/api/moisture/route.ts
+++ app/src/app/api/moisture/route.ts
@@ -2,46 +2,37 @@ import { NextRequest, NextResponse } from 'next/server';
 import dbConnect from '@/lib/dbConnect'; // Ensure the path to your dbConnect is correct
 import MoistureModel from '@/models/Moisture'; // Ensure the path to your Moisture model is correct
 
-
 export async function GET(req: NextRequest) {
   try {
     // Ensure db connection
     await dbConnect();
 
-    // Check if moisture query parameter is provided for saving
+    // Extract query parameters directly from the request object (nextUrl is more Next.js-friendly)
+    const { searchParams } = req.nextUrl;
+    const moistureBefore = searchParams.get('moistureBefore');
+    const moistureAfter = searchParams.get('moistureAfter');
+    const isIrrigated = searchParams.get('isIrrigated');
 
-    if (req.method === 'GET' ) {
-          const { searchParams } = new URL(req.url);
-          const moistureBefore = searchParams.get('moistureBefore');
-          const moistureAfter = searchParams.get('moistureAfter');
-          const isIrrigated = searchParams.get('isIrrigated');
+    if (!moistureBefore || !moistureAfter || !isIrrigated) {
+      return NextResponse.json(
+        { success: false, message: 'Moisture content is required' },
+        { status: 400 }
+      );
+    }
 
-          if (!moistureBefore || !moistureAfter || !isIrrigated) {
-              return NextResponse.json(
-                { success: false, message: 'Moisture content is required' },
-                { status: 400 } )
-              ;}
+    // Create a new moisture record
+    const moistureData = await MoistureModel.create({
+      moistureBefore: Number(moistureBefore),
+      moistureAfter: Number(moistureAfter),
+      isIrrigated: Number(isIrrigated),
+      timestamp: new Date(),
+    });
 
-            const moistureData = await MoistureModel.create({
-              moistureBefore: Number(moistureBefore),
-              moistureAfter: Number(moistureAfter),
-              isIrrigated: Number(isIrrigated),
-              timestamp: new Date(),
-            });
-                        
-
-            return NextResponse.json(
-              { success: true, data: moistureData },
-              { status: 201 }
-            );
-
-
-
-}
-
-  }
-    
-   catch (error) {
+    return NextResponse.json(
+      { success: true, data: moistureData },
+      { status: 201 }
+    );
+  } catch (error) {
     console.error('Error handling moisture data:', error);
     return NextResponse.json(
       { success: false, message: 'Internal Server Error' },
@@ -49,4 +40,3 @@ export async function GET(req: NextRequest) {
     );
   }
 }
-
--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,22 +1,20 @@
-"use client";
+'use client';
 
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-import { Progress } from "react-circle-progress-bar";
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { Progress } from 'react-circle-progress-bar';
 
-import Image from "next/image"
-import { format, formatRelative } from 'date-fns';
+import Image from 'next/image';
+import { formatRelative } from 'date-fns';
 
-import { eu } from 'date-fns/locale';
-import { ResponsiveLine } from '@nivo/line'
+import { ResponsiveLine } from '@nivo/line';
 
 const fetchMoistureData = async () => {
-  const response = await axios.get("/api/getmoisture");
+  const response = await axios.get('/api/getmoisture');
   return response.data.data; // Assuming the data is in the 'data' field
 };
 
 const progressStyles = {};
-
 
 const isMoreThan3DaysAgo = (timestamp: string): boolean => {
   const threeDaysInMilliseconds = 3 * 24 * 60 * 60 * 1000; // 3 days in milliseconds
@@ -26,160 +24,182 @@ const isMoreThan3DaysAgo = (timestamp: string): boolean => {
   return currentTimestamp - givenTimestamp > threeDaysInMilliseconds;
 };
 
-
 export default function Home() {
-
   const getMoistureQuery = {
-    queryKey: ["moisture"],
+    queryKey: ['moisture'],
     queryFn: () => fetchMoistureData(),
   };
 
   const { data, isLoading, error } = useQuery(getMoistureQuery);
 
-
-  const graphData = !isLoading && !error && [{
-    "id": "mostureGraph",
-    "color": "hsl(184, 70%, 50%)",
-    "data": data.map(({ timestamp, moistureAfter }) => ({
-    // "x": format(timestamp, "dd/MM/yyyy,H:mm"),          
-    "x": timestamp,          
-    "y": moistureAfter       
-  }))}]
-
-
-  console.log
+  const graphData = !isLoading &&
+    !error && [
+      {
+        id: 'mostureGraph',
+        color: 'hsl(184, 70%, 50%)',
+        data: data.map(({ timestamp, moistureAfter }) => ({
+          // "x": format(timestamp, "dd/MM/yyyy,H:mm"),
+          x: timestamp,
+          y: moistureAfter,
+        })),
+      },
+    ];
 
   return (
     <>
-    <p className="p-4 pl-8 bold font-bold text-xl text-blue-600">Plantly<small className="text-slate-600 text-xs ml-1 font-thin">Dashboard</small></p>
+      <p className="p-4 pl-8 bold font-bold text-xl text-blue-600">
+        Plantly
+        <small className="text-slate-600 text-xs ml-1 font-thin">
+          Dashboard
+        </small>
+      </p>
       <hr />
-    <div className="m-8 max-w-[690px] mx-auto p-2 flex flex-wrap gap-8 font-[family-name:var(--font-geist-sans)] ">
-      
-      {isLoading && <p className="m-auto">Loading...</p>}
-      {error && <p className="m-auto text-red-600 text-center">There was an error<i>please refresh</i></p>}
-      {!isLoading && !error && data && (
-      <div className="border-2 mx-auto border-slate-300 p-2 rounded-lg text-center">
-        <h4 className="bg-blue-500 font-light text-white p-1 px-3 rounded-2xl w-fit text-sm mx-auto">Moisture Before</h4>
-          <Progress
-          reduction={0}
-          transitionDuration={0.5}
-          transitionTimingFunction={"ease"}
-            gradient={[
-              { stop: 0.0, color: "#00bc9b" },
-              { stop: 1, color: "#5eaefd" },
-            ]}
-            style={progressStyles}
-            progress={data.at(-1).moistureBefore}
-          />
-      </div>
+      <div className="m-8 max-w-[690px] mx-auto p-2 flex flex-wrap gap-8 font-[family-name:var(--font-geist-sans)] ">
+        {isLoading && <p className="m-auto">Loading...</p>}
+        {error && (
+          <p className="m-auto text-red-600 text-center">
+            There was an error<i>please refresh</i>
+          </p>
         )}
-
-
-      {!isLoading && !error && data && (
-      <div className="border-2 mx-auto border-slate-300 p-2 rounded-lg text-center">
-        <h4 className="bg-blue-500 font-light text-white p-1 px-3 rounded-2xl w-fit text-sm mx-auto">Moisture After</h4>
-        
-          <Progress
-          reduction={0}
-          transitionDuration={0.5}
-          transitionTimingFunction={"ease"}
-            gradient={[
-              { stop: 0.0, color: "#00bc9b" },
-              { stop: 1, color: "#5eaefd" },
-            ]}
-            style={progressStyles}
-            progress={data.at(-1).moistureAfter}
-          />
-      </div>
+        {!isLoading && !error && data && (
+          <div className="border-2 mx-auto border-slate-300 p-2 rounded-lg text-center">
+            <h4 className="bg-blue-500 font-light text-white p-1 px-3 rounded-2xl w-fit text-sm mx-auto">
+              Moisture Before
+            </h4>
+            <Progress
+              reduction={0}
+              transitionDuration={0.5}
+              transitionTimingFunction={'ease'}
+              gradient={[
+                { stop: 0.0, color: '#00bc9b' },
+                { stop: 1, color: '#5eaefd' },
+              ]}
+              style={progressStyles}
+              progress={data.at(-1).moistureBefore}
+            />
+          </div>
         )}
 
         {!isLoading && !error && data && (
-      <div className="border-2 border-slate-300 p-2 rounded-lg text-center">
-        <h4 className="flex bg-blue-500 font-light text-white p-1 px-3 rounded-2xl w-fit text-sm mr-auto ml-auto">Plant Mood</h4>
-        <Image alt="happyplant" src={isMoreThan3DaysAgo(data.at(-1).timestamp) ? "/sad.png" : "/happy.png"} width={120} height={160}/>
-      </div>
+          <div className="border-2 mx-auto border-slate-300 p-2 rounded-lg text-center">
+            <h4 className="bg-blue-500 font-light text-white p-1 px-3 rounded-2xl w-fit text-sm mx-auto">
+              Moisture After
+            </h4>
+
+            <Progress
+              reduction={0}
+              transitionDuration={0.5}
+              transitionTimingFunction={'ease'}
+              gradient={[
+                { stop: 0.0, color: '#00bc9b' },
+                { stop: 1, color: '#5eaefd' },
+              ]}
+              style={progressStyles}
+              progress={data.at(-1).moistureAfter}
+            />
+          </div>
         )}
 
-      {!isLoading && !error && data && (
-        <div className="w-[450px] h-96">
-        <ResponsiveLine
-        data={graphData}
-        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-        xScale={{ type: 'point' }}
-        yScale={{
-            type: 'linear',
-            min: 'auto',
-            max: 'auto',
-            stacked: true,
-            reverse: false
-        }}
-        yFormat=" >-.2f"
-        axisTop={null}
-        axisRight={null}
-        axisBottom={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'Time',
-            legendOffset: 36,
-            legendPosition: 'middle',
-            truncateTickAt: 0
-        }}
-        axisLeft={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'Moisture content',
-            legendOffset: -40,
-            legendPosition: 'middle',
-            truncateTickAt: 0
-        }}
-        pointSize={10}
-        pointColor={{ theme: 'background' }}
-        pointBorderWidth={2}
-        pointBorderColor={{ from: 'serieColor' }}
-        pointLabel="data.yFormatted"
-        pointLabelYOffset={-12}
-        enableTouchCrosshair={true}
-        useMesh={true}
-        legends={[
-            {
-                anchor: 'bottom-right',
-                direction: 'column',
-                justify: false,
-                translateX: 100,
-                translateY: 0,
-                itemsSpacing: 0,
-                itemDirection: 'left-to-right',
-                itemWidth: 80,
-                itemHeight: 20,
-                itemOpacity: 0.75,
-                symbolSize: 12,
-                symbolShape: 'circle',
-                symbolBorderColor: 'rgba(0, 0, 0, .5)',
-                effects: [
-                    {
-                        on: 'hover',
-                        style: {
-                            itemBackground: 'rgba(0, 0, 0, .03)',
-                            itemOpacity: 1
-                        }
-                    }
-                ]
-            }
-        ]}
-    />
-    </div>
-  
-  )}
-      {!isLoading && !error && data && (
-      <div className="grid mx-auto h-fit">
-      <h4 className="flex bg-blue-500 font-light text-white p-1 px-3 rounded-2xl w-fit text-sm h-fit">Last Irrigation</h4>
-      <p className="font-light text-5xl text-slate-600 mt-2">{data && formatRelative(data?.at(-1).timestamp , new Date()) }</p>
+        {!isLoading && !error && data && (
+          <div className="border-2 border-slate-300 p-2 rounded-lg text-center">
+            <h4 className="flex bg-blue-500 font-light text-white p-1 px-3 rounded-2xl w-fit text-sm mr-auto ml-auto">
+              Plant Mood
+            </h4>
+            <Image
+              alt="happyplant"
+              src={
+                isMoreThan3DaysAgo(data.at(-1).timestamp)
+                  ? '/sad.png'
+                  : '/happy.png'
+              }
+              width={120}
+              height={160}
+            />
+          </div>
+        )}
 
-    </div>)}
-    </div>
-  
+        {!isLoading && !error && data && (
+          <div className="w-[450px] h-96">
+            <ResponsiveLine
+              data={graphData}
+              margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+              xScale={{ type: 'point' }}
+              yScale={{
+                type: 'linear',
+                min: 'auto',
+                max: 'auto',
+                stacked: true,
+                reverse: false,
+              }}
+              yFormat=" >-.2f"
+              axisTop={null}
+              axisRight={null}
+              axisBottom={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: 'Time',
+                legendOffset: 36,
+                legendPosition: 'middle',
+                truncateTickAt: 0,
+              }}
+              axisLeft={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: 'Moisture content',
+                legendOffset: -40,
+                legendPosition: 'middle',
+                truncateTickAt: 0,
+              }}
+              pointSize={10}
+              pointColor={{ theme: 'background' }}
+              pointBorderWidth={2}
+              pointBorderColor={{ from: 'serieColor' }}
+              pointLabel="data.yFormatted"
+              pointLabelYOffset={-12}
+              enableTouchCrosshair={true}
+              useMesh={true}
+              legends={[
+                {
+                  anchor: 'bottom-right',
+                  direction: 'column',
+                  justify: false,
+                  translateX: 100,
+                  translateY: 0,
+                  itemsSpacing: 0,
+                  itemDirection: 'left-to-right',
+                  itemWidth: 80,
+                  itemHeight: 20,
+                  itemOpacity: 0.75,
+                  symbolSize: 12,
+                  symbolShape: 'circle',
+                  symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                  effects: [
+                    {
+                      on: 'hover',
+                      style: {
+                        itemBackground: 'rgba(0, 0, 0, .03)',
+                        itemOpacity: 1,
+                      },
+                    },
+                  ],
+                },
+              ]}
+            />
+          </div>
+        )}
+        {!isLoading && !error && data && (
+          <div className="grid mx-auto h-fit">
+            <h4 className="flex bg-blue-500 font-light text-white p-1 px-3 rounded-2xl w-fit text-sm h-fit">
+              Last Irrigation
+            </h4>
+            <p className="font-light text-5xl text-slate-600 mt-2">
+              {data && formatRelative(data?.at(-1).timestamp, new Date())}
+            </p>
+          </div>
+        )}
+      </div>
     </>
   );
 }
