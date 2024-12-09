8858d67bada2ce90e797617a182cda66fec2a845
ibrahim abdulganiy <mayowaibrahim009@gmail.com>
2024-12-09 13:58:11 +0200

    fix server caching

--- app/src/app/api/getmoisture/route.ts
+++ app/src/app/api/getmoisture/route.ts
@@ -2,29 +2,27 @@ import { NextRequest, NextResponse } from 'next/server';
 import dbConnect from '@/lib/dbConnect'; // Ensure the path to your dbConnect is correct
 import MoistureModel from '@/models/Moisture'; // Ensure the path to your Moisture model is correct
 
-
 export async function GET(req: NextRequest) {
   try {
     // Ensure db connection
     await dbConnect();
 
-    // Check if moisture query parameter is provided for saving
+    // Fetch moisture data
+    const moistureData = await MoistureModel.find({});
 
-    if (req.method === 'GET' ) {
-
-            const moistureData = await MoistureModel.find({});
-            return NextResponse.json(
-              { success: true, data: moistureData },
-              { status: 200 }
-          );
-
-
-
-}
-
-  }
-    
-   catch (error) {
+    // Return response with no-cache headers
+    return NextResponse.json(
+      { success: true, data: moistureData },
+      {
+        status: 200,
+        headers: {
+          'Cache-Control': 'no-store', // Prevent any caching
+          Pragma: 'no-cache',
+          Expires: '0', // Ensure expiration is set to 0 to prevent caching
+        },
+      }
+    );
+  } catch (error) {
     console.error('Error handling moisture data:', error);
     return NextResponse.json(
       { success: false, message: 'Internal Server Error' },
@@ -32,4 +30,3 @@ export async function GET(req: NextRequest) {
     );
   }
 }
-
